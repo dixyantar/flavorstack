88bcf43b20d10bb5ff9241c3dcc6d4b79f4bbc91
Dixyantar Panda <dixyantar@MacBook-Pro.local>
2020-06-29 09:58:33 -0500

    google auth

--- flavorstack/src/main/java/com/flavorstack/app/BaseRestController.java
+++ flavorstack/src/main/java/com/flavorstack/app/BaseRestController.java
@@ -3,15 +3,35 @@ package com.flavorstack.app;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.flavorstack.app.auth.GoogleAuth;
 
 @RestController
 public class BaseRestController {
 
-	
+	private GoogleAuth gAuth;
+
+	public BaseRestController() throws Exception {
+		gAuth = new GoogleAuth();
+	}
+
 	@GetMapping("/get")
-	public ResponseEntity<String> get(){
+	public ResponseEntity<String> get() {
 		String resp = "Auto Build Success";
-		return new ResponseEntity<>(resp,HttpStatus.OK);
+		return new ResponseEntity<>(resp, HttpStatus.OK);
+	}
+
+	@PostMapping("/authSuccess")
+	public ResponseEntity<String> authSucess(@RequestParam String idtoken) throws Exception {
+		
+		if(gAuth.isVerified(idtoken)) {
+			new ResponseEntity<>("Work done on auth Success", HttpStatus.OK);
+		}else {
+			new ResponseEntity<>("Auth Unsuccessfull", HttpStatus.UNAUTHORIZED);
+		}
+		return null;
 	}
 }
---
+++ flavorstack/src/main/java/com/flavorstack/app/auth/GoogleAuth.java
@@ -0,0 +1,51 @@
+package com.flavorstack.app.auth;
+
+import java.util.Collections;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+
+public class GoogleAuth {
+
+	private GoogleIdTokenVerifier verifier;
+	private static final String CLIENT_ID = "70335511196-acnuatmjb07pbcftigiqfh2th7rk4o2c.apps.googleusercontent.com";
+
+	public GoogleAuth() throws Exception {
+		HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+		JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+		this.verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+				.setAudience(Collections.singletonList(CLIENT_ID)).build();
+	}
+	
+	public boolean isVerified(String idTokenString) throws Exception {
+		boolean isVerified = false;
+		GoogleIdToken idToken = verifier.verify(idTokenString);
+		if (idToken != null) {
+		  Payload payload = idToken.getPayload();
+
+		  // Print user identifier
+		  String userId = payload.getSubject();
+		  System.out.println("User ID: " + userId);
+
+		  // Get profile information from payload
+		  String email = payload.getEmail();
+		  boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+		  String name = (String) payload.get("name");
+		  String pictureUrl = (String) payload.get("picture");
+		  String locale = (String) payload.get("locale");
+		  String familyName = (String) payload.get("family_name");
+		  String givenName = (String) payload.get("given_name");
+
+		  System.out.println(email);
+		  isVerified = true;
+
+		} 
+		return isVerified;
+	}
+
+}
